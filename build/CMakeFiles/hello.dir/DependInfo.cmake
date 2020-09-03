723e1a33a5ec8c20d9ec416ab6fa1300293af462
jneirar <jorge.neira@utec.edu.pe>
2020-09-03 08:40:20 -0500

    First commit

---
+++ build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jneira/Insync/UTEC/UTEC/05 - 2020-2/Algoritmos y Estructura de datos/S01/Hello/src/main.cpp" "/home/jneira/Insync/UTEC/UTEC/05 - 2020-2/Algoritmos y Estructura de datos/S01/Hello/build/CMakeFiles/hello.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
